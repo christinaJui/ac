b224b3b64154aa2046d5e59e6712cff1373c1965
christinaJui <christina.arinda@aamra.com.bd>
2019-04-17 11:08:39 +0600

    add file

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <!-- Add custom CSS here -->
+        <style type="text/css">
+.ui-widget{text-align: center;margin-top: 35px;}.ui-autocomplete{ text-align:left !important;}
+#skills{height: 25px; font-size: 16px;}
+</style>
+</head>
+<body>
+       
+
+<div class="demo-title"><h4>DEMO BY <span class="one">CODEX</span><span class="two">WORLD</span>: Autocomplete Textbox using jQuery, PHP and MySQL</h4></div>
+        <div class="container">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div>
+              <div class="ui-widget">
+                <label for="skills">Your Skills: </label>
+                <input id="skills">
+              </div>
+	    </div>
+        </div>
+    </div>
+
+</div>
+<p>ristina jui</p>   
+        	<!-- JavaScript -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+     	    	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
+        
+    	    <script>
+  $(function() {  
+    $( "#skills" ).autocomplete({
+      source: "search.php"
+    });
+  });
+  </script>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+
+// Database configuration
+$dbHost     = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName     = 'ittat_studio';
+
+// Connect with the database
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// Get search term
+$searchTerm = $_REQUEST['term'];
+
+// Get matched data from skills table
+$query = $db->query("SELECT * FROM category WHERE category_name LIKE '%".$searchTerm."%' ORDER BY category_name ASC");
+
+// Generate skills data array
+$skillData = array();
+if($query->num_rows > 0){
+    while($row = $query->fetch_assoc()){
+        $data['value'] = $row['category_name'];
+        array_push($skillData, $data);
+    }
+}
+
+// Return results as json encoded array
+echo json_encode($skillData);
+
+?>
